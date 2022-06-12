abffac826e6329c91ed19cc2ad25d13258a99d05
Parabeac-Nest <admin@parabeac.com>
2022-06-12 17:55:09 +0000

    Auto-generated commit by Parabeac Nest

---
+++ lib/views/page_1/i_phone11_pro_x_5.g.dart
@@ -0,0 +1,296 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+//
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+
+import 'package:flutter/material.dart';
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:tripify/widgets/components/o_b_forms_sign_up/property1_hover.g.dart';
+import 'package:tripify/widgets/page_1/arrow_alt_left.g.dart';
+
+class IPhone11ProX5 extends StatefulWidget {
+  const IPhone11ProX5({
+    Key? key,
+  }) : super(key: key);
+  @override
+  _IPhone11ProX5 createState() => _IPhone11ProX5();
+}
+
+class _IPhone11ProX5 extends State<IPhone11ProX5> {
+  _IPhone11ProX5();
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.white,
+      child: Stack(children: [
+        Positioned(
+          left: 0,
+          width: 375.0,
+          top: 15.0,
+          height: 812.0,
+          child: Image.asset(
+            'assets/images/4411220eac663a3751cdfc1f75864ad02.png',
+            package: 'tripify',
+            width: 375.000,
+            height: 812.000,
+            fit: BoxFit.none,
+          ),
+        ),
+        Positioned(
+          left: 0,
+          width: 375.0,
+          top: 15.0,
+          height: 812.0,
+          child: Container(
+            width: 375.000,
+            height: 812.000,
+            decoration: BoxDecoration(
+              color: Color(0xe5fdc100),
+            ),
+          ),
+        ),
+        Positioned(
+          left: 111.0,
+          width: 152.0,
+          top: 712.0,
+          height: 64.0,
+          child: LayoutBuilder(builder: (context, constraints) {
+            return Property1Hover(
+              constraints,
+              ovrName: 'Sign Up',
+            );
+          }),
+        ),
+        Positioned(
+          left: 27.0,
+          width: 320.0,
+          top: 256.0,
+          height: 53.0,
+          child: Container(
+            width: 320.000,
+            height: 53.000,
+            decoration: BoxDecoration(
+              color: Colors.white,
+            ),
+          ),
+        ),
+        Positioned(
+          left: 28.0,
+          width: 93.0,
+          top: 228.0,
+          height: 28.0,
+          child: Container(
+              width: 93.000,
+              height: 28.000,
+              child: AutoSizeText(
+                'First Name',
+                style: TextStyle(
+                  fontFamily: 'Pragati Narrow',
+                  fontSize: 25,
+                  fontWeight: FontWeight.w400,
+                  letterSpacing: 0,
+                  color: Colors.black,
+                ),
+                textAlign: TextAlign.center,
+              )),
+        ),
+        Positioned(
+          left: 27.0,
+          width: 320.0,
+          top: 358.0,
+          height: 53.0,
+          child: Container(
+            width: 320.000,
+            height: 53.000,
+            decoration: BoxDecoration(
+              color: Colors.white,
+            ),
+          ),
+        ),
+        Positioned(
+          left: 28.0,
+          width: 92.0,
+          top: 330.0,
+          height: 28.0,
+          child: Container(
+              width: 92.000,
+              height: 28.000,
+              child: AutoSizeText(
+                'Last Name',
+                style: TextStyle(
+                  fontFamily: 'Pragati Narrow',
+                  fontSize: 25,
+                  fontWeight: FontWeight.w400,
+                  letterSpacing: 0,
+                  color: Colors.black,
+                ),
+                textAlign: TextAlign.center,
+              )),
+        ),
+        Positioned(
+          left: 27.0,
+          width: 320.0,
+          top: 460.0,
+          height: 53.0,
+          child: Container(
+            width: 320.000,
+            height: 53.000,
+            decoration: BoxDecoration(
+              color: Colors.white,
+            ),
+          ),
+        ),
+        Positioned(
+          left: 28.0,
+          width: 68.0,
+          top: 432.0,
+          height: 28.0,
+          child: Container(
+              width: 68.000,
+              height: 28.000,
+              child: AutoSizeText(
+                'NIC No.',
+                style: TextStyle(
+                  fontFamily: 'Pragati Narrow',
+                  fontSize: 25,
+                  fontWeight: FontWeight.w400,
+                  letterSpacing: 0,
+                  color: Colors.black,
+                ),
+                textAlign: TextAlign.center,
+              )),
+        ),
+        Positioned(
+          left: 27.0,
+          width: 320.0,
+          top: 562.0,
+          height: 53.0,
+          child: Container(
+            width: 320.000,
+            height: 53.000,
+            decoration: BoxDecoration(
+              color: Colors.white,
+            ),
+          ),
+        ),
+        Positioned(
+          left: 30.0,
+          width: 65.0,
+          top: 534.0,
+          height: 28.0,
+          child: Container(
+              width: 65.000,
+              height: 28.000,
+              child: AutoSizeText(
+                'Contact',
+                style: TextStyle(
+                  fontFamily: 'Pragati Narrow',
+                  fontSize: 25,
+                  fontWeight: FontWeight.w400,
+                  letterSpacing: 0,
+                  color: Colors.black,
+                ),
+                textAlign: TextAlign.center,
+              )),
+        ),
+        Positioned(
+          left: 0,
+          width: 375.0,
+          top: 0,
+          height: 95.0,
+          child: Container(
+              width: 375.000,
+              height: 95.000,
+              decoration: BoxDecoration(),
+              child: Stack(children: [
+                Positioned(
+                  left: 0,
+                  width: 375.0,
+                  top: 15.0,
+                  height: 68.0,
+                  child: Container(
+                    width: 375.000,
+                    height: 68.000,
+                    decoration: BoxDecoration(
+                      color: Color(0xff202020),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  left: 21.0,
+                  width: 43.0,
+                  top: 28.0,
+                  height: 43.0,
+                  child: Image.asset(
+                    'assets/images/ellipse1.png',
+                    package: 'tripify',
+                    width: 43.000,
+                    height: 43.000,
+                    fit: BoxFit.none,
+                  ),
+                ),
+                Positioned(
+                  left: 23.0,
+                  width: 41.0,
+                  top: 29.0,
+                  height: 41.0,
+                  child: LayoutBuilder(builder: (context, constraints) {
+                    return ArrowAltLeft(
+                      constraints,
+                      ovrVector9: SvgPicture.asset(
+                        'assets/images/vector9.svg',
+                        package: 'tripify',
+                        width: MediaQuery.of(context).size.width * 0.055,
+                        height: MediaQuery.of(context).size.height * 0.017,
+                        fit: BoxFit.fill,
+                      ),
+                    );
+                  }),
+                ),
+                Positioned(
+                  left: 140.0,
+                  width: 95.0,
+                  top: 0,
+                  height: 95.0,
+                  child: Image.asset(
+                    'assets/images/nam1.png',
+                    package: 'tripify',
+                    width: 95.000,
+                    height: 95.000,
+                    fit: BoxFit.none,
+                  ),
+                ),
+              ])),
+        ),
+        Positioned(
+          left: 44.0,
+          width: 286.0,
+          top: 150.0,
+          height: 33.0,
+          child: Container(
+              width: 286.000,
+              height: 33.000,
+              child: AutoSizeText(
+                'Create Your Account',
+                style: TextStyle(
+                  fontFamily: 'Roboto Mono',
+                  fontSize: 25,
+                  fontWeight: FontWeight.w400,
+                  letterSpacing: 0,
+                  color: Colors.black,
+                ),
+                textAlign: TextAlign.left,
+              )),
+        ),
+      ]),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+}
---
+++ lib/widgets/page_1/arrow_alt_left.g.dart
@@ -0,0 +1,51 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+//
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class ArrowAltLeft extends StatefulWidget {
+  final BoxConstraints constraints;
+  final Widget? ovrVector9;
+  const ArrowAltLeft(
+    this.constraints, {
+    Key? key,
+    this.ovrVector9,
+  }) : super(key: key);
+  @override
+  _ArrowAltLeft createState() => _ArrowAltLeft();
+}
+
+class _ArrowAltLeft extends State<ArrowAltLeft> {
+  _ArrowAltLeft();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        decoration: BoxDecoration(),
+        child: Stack(children: [
+          Positioned(
+            left: 8.542,
+            width: 20.5,
+            top: 13.667,
+            height: 13.667,
+            child: widget.ovrVector9 ??
+                SvgPicture.asset(
+                  'assets/images/vector9.svg',
+                  package: 'tripify',
+                  width: widget.constraints.maxWidth * 0.500,
+                  height: widget.constraints.maxHeight * 0.333,
+                  fit: BoxFit.none,
+                ),
+          ),
+        ]));
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+}
